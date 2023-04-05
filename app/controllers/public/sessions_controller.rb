229b3ef028345a3e3fa4454c5d7756de02b6cfea
kitajimakokoro <122000079+kitajimakokoro@users.noreply.github.com>
2023-04-05 11:48:42 +0000

    [Add][user/admin]devise導入・ログイン機能の実装

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  before_action :user_state, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  protected
+
+ #退会しているか判断するメソッド
+def user_state
+  #Userモデルから入力されたemailを検索し該当アカウントを1件取得
+  @user = User.find_by(email: params[:user][:email])
+  # アカウントを取得できなかった場合、このメソッドを終了する
+  return if !@user
+    #取得したアカウントのパスワードと入力されたパスワードが一致してるか && 取得したユーザーの退会ステータスを判別
+  if @user.valid_password?(params[:user][:password]) && @user.is_deleted == true
+    flash[:notice] = "退会済みの為、再登録が必要です。"
+    #上記どちらかでもtrueだった場合、サインアップ画面に遷移する
+    redirect_to new_user_registration_path
+  end
+end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+
+
+
+  namespace :admin do
+    root to: 'homes#top'
+  end
+
+  scope module: :public do
+    root to: "homes#top"
+  end
+
+
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
